615ac8b5364f26d15d5cdcbba86d825cfb7e0c1b
Tonnystark20 <tonnyndima20@gmail.com>
2023-02-11 00:50:08 +0300

    whatsapp drag

--- js/main.js
+++ js/main.js
@@ -185,7 +185,6 @@ const data = [
     { name: "   Contact ", url: "contact.html" },
     { name: "C.O.O Afrobeta Consult", url: "team.html#kwamboka" },
     { name: "Creative Director AfroBeta Consult", url: "team.html#Nyaga" },
-
     { name: " Feedback", url: "contact.html#email" },
     { name: "Emmanuel Misiati Marumbu", url: "team.html#founder" },
     { name: "Facilitation ", url: "service.html#facilitation" },
@@ -201,16 +200,11 @@ const data = [
     { name: " Group Engagement ", url: "price.html#consultation" },
     { name: " Engagement ", url: "price.html#facilitation" },
     { name: " Experience Curation ", url: "price.html#facilitation" },
-
-
     { name: " Services", url: "service.html#services" },
-
     { name: "Solutions ", url: "services.html" },
-
     { name: " Clients ", url: "index.html#clients" },
     { name: " Program Curation ", url: "price.html#consultation" },
     { name: " Personal & Team leadership ", url: "price.html#consultation" },
-
     { name: "Recent Posts ", url: "blog.html#recent" },
     { name: "Join Shiriki", url: "blog2.html#teule" },
     { name: "Susan Makori Kwamboka", url: "team.html#kwamboka" },
@@ -249,3 +243,48 @@ searchIcon.addEventListener("click", () => {
         window.location = selectedItem.url;
     }
 });
+// whatsapp draggerble 
+
+
+interact(document.getElementById("whatsapp-chat"))
+    .draggable({
+        // enable inertial throwing
+        inertia: true,
+        // keep the element within the area of it's parent
+        restrict: {
+            restriction: "parent",
+            endOnly: true,
+            elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+        },
+        // enable autoScroll
+        autoScroll: true,
+
+        // call this function on every dragmove event
+        onmove: dragMoveListener,
+        // call this function on every dragend event
+        onend: function (event) {
+            var textEl = event.target.querySelector("p");
+
+            textEl &&
+                (textEl.textContent =
+                    "moved a distance of " +
+                    (Math.sqrt(Math.pow(event.pageX - event.x0, 2) + Math.pow(event.pageY - event.y0, 2) | 0))
+                        .toFixed(2) +
+                    "px");
+        }
+    });
+
+function dragMoveListener(event) {
+    var target = event.target,
+        // keep the dragged position in the data-x/data-y attributes
+        x = (parseFloat(target.getAttribute("data-x")) || 0) + event.dx,
+        y = (parseFloat(target.getAttribute("data-y")) || 0) + event.dy;
+
+    // translate the element
+    target.style.webkitTransform = target.style.transform =
+        "translate(" + x + "px, " + y + "px)";
+
+    // update the posiion attributes
+    target.setAttribute("data-x", x);
+    target.setAttribute("data-y", y);
+}
